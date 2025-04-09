b988f271c051e4be2c57a13d6380654794aaa20c
avilov1022 <118724722+avilov1022@users.noreply.github.com>
2025-04-09 23:18:33 +0500

    ФИО финал

--- resources/views/components/admin-card.blade.php
+++ resources/views/components/admin-card.blade.php
@@ -1,8 +1,13 @@
-<div class="flex justify-center items-center bg-gray-100">
+<div class="flex justify-center items-center bg-gray-100 mt-3">
     <div class="bg-white shadow-md rounded-lg p-4 max-w-md w-full mb-2">
         <h2 class="font-bold text-xl mb-2">Заявка от {{ $report->created_at ? $report->created_at->format('d.m.Y') : 'Неизвестное время' }}</h2>
-        <p><strong>Пользователь:</strong> {{ $report->user->name ?? 'Неизвестный пользователь' }}</p> 
+        <p><strong>Пользователь:</strong> {{ $report->user->FullName() ?? 'Неизвестный пользователь' }}</p> 
         <p><strong>Тема:</strong> {{ $report-> theme }}</p>
-        <p><img src="{{ asset('image/'. $report->path_img) }}" alt="profile Pic" height="200" width="200">        </p>
+        <p><img src="{{ asset('image/'. $report->path_img) }}" alt="profile Pic" height="200" width="200"></p>
+
+        <select name="" id="" class="mt-4">
+            <option value="">Одобрить</option>
+            <option value="">Отклонить</option>
+        </select>
     </div>
 </div>
